1bbad5a12c88aab85fb7c38bc38461d02bcefe1c
Roswall <simonroswall@gmail.com>
2021-04-08 11:16:18 +0200

    startet cuttinggrass problem

---
+++ GitGroupAflevering/src/problemSet/CuttingGrass.java
@@ -0,0 +1,13 @@
+package problemSet;
+import java.util.Scanner;
+
+public class CuttingGrass{
+    private final double GROWTH = 0.8d;
+    private final Scanner sc = new Scanner(System.in);
+
+
+    public double method (double currentLength, double minLength ){
+
+        return days;
+    }
+}
